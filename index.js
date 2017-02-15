a9bf9dbf90ee82ea90d9608d8f3da537523e8873
Biser Perchinkov <biser@bisko.be>
2017-02-15 11:53:59 +0200

    Add random meeting pairings for team members

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const micro = require( 'micro' );
-const { always, join, map, pipe, range, take } = require( 'ramda' );
-const { shuffle } = require( 'lodash' );
+const { always, concat, join, map, pipe, range, take } = require( 'ramda' );
+const { reject, shuffle } = require( 'lodash' );
 
 const {
 	channelName,
 	cribbsPerWeek,
+	oneOnOnesPerWeek,
 	teamMembers,
 } = require( './config' );
 
@@ -15,6 +16,55 @@ const weekdays = [
 	'Friday',
 ];
 
+const getRandomMeetingsForTheWeek = () => {
+	let oneOnOnes = {};
+
+	do {
+		oneOnOnes = {};
+		teamMembers.map( ( name ) => {
+			oneOnOnes[ name ] = [];
+		} );
+
+		shuffle( teamMembers ).map( ( name ) => {
+			const possibleMeetings = shuffle( reject( teamMembers, ( member ) => (name === member) ) );
+
+			possibleMeetings.map( ( member ) => {
+				if (
+					Math.max( // check if either team member has had enough meetings for the week.
+						oneOnOnes[ name ].length,
+						oneOnOnes[ member ].length
+					) >= oneOnOnesPerWeek
+					|| oneOnOnes[ name ].indexOf( member ) !== - 1 // crosscheck if member and name already have a meeting
+					|| oneOnOnes[ member ].indexOf( name ) !== - 1
+				) {
+					return;
+				}
+
+				oneOnOnes[ name ] = [ ...oneOnOnes[ name ], member ];
+				oneOnOnes[ member ] = [ ...oneOnOnes[ member ], name ];
+			} );
+
+		} );
+
+		/**
+		 * Because of the algorithm randomness sometimes some people don't have enough meetings throughout the week.
+		 * This loop makes sure everyone has enough meetings for the week.
+		 */
+	} while ( reject( oneOnOnes, ( meetings ) => (
+		meetings.length < oneOnOnesPerWeek
+	) ).length != teamMembers.length );
+
+	return oneOnOnes;
+};
+
+const convertMeetingsToText = ( meetings ) => (
+	teamMembers.map( ( member ) => (
+		`${member} is meeting with: ${meetings[ member ].join( ', ' )}`
+	) )
+);
+
+const meetingsText = [ 'Meetings for the week!\n', ...convertMeetingsToText( getRandomMeetingsForTheWeek() ) , '\n' ];
+
 const timeslots = range( 11, 19 ).map( hh => `${hh}:00` );
 
 const draw = pipe( shuffle, take( 1 ) );
@@ -24,7 +74,9 @@ const reminders = pipe(
 	shuffle,
 	take( cribbsPerWeek ),
 	map( day => `/remind ${channelName} Cribbs-Fonseca pairing! ${teamMembers.join()} at ${draw(timeslots)} on ${day}` ),
-	join( '\n' ) );
+	concat( meetingsText ),
+	join( '\n' )
+);
 
 const server = micro( async (req, res) => {
 	res.writeHead( 200 )
